7398224b4f5be6f76b120f8d6a9a13221b398d7e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-01 11:25:02 +0000

    build based on fbc1f1f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DeepViz.jl-1","page":"Home","title":"DeepViz.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [DeepViz]","category":"page"}]
+}
