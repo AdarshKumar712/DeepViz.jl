ff34322d2e58fe8f76a0275d5bdd585838985b47
Adarshkumar712 <Adarshkumar712.ak@gmail.com>
2020-06-02 21:58:59 +0530

    Added documentation for utility functions and vanilla backprop

--- src/utils.jl
+++ src/utils.jl
@@ -1,22 +1,36 @@
-im2arr_rgb(img) = permutedims(float.(channelview(imresize(img, (224, 224)))), (3, 2, 1))
 im_mean = reshape([0.485, 0.456, 0.406], 1, 1, 3)
+global original_size = (224, 224)
 
+im2arr_rgb(img) = permutedims(float.(channelview(imresize(img, (224, 224)))), (3, 2, 1))
+
+"""
+    set_training_false()
+
+Function to set the model into `testmode`, so that the output result do not vary while evaluating gradients(or other results) for the models which contain layers like `BatchNormalization`, `Dropout` etc.
+"""
 function set_training_false() 
     @eval Flux.istraining() = false
     @eval Zygote.@adjoint Flux.istraining() = false, _ -> nothing
 end
 
+"""
+    set_training_true()
+
+Function to revert back the model into `trainmode`, once the gradients(or other results) are evaluated.
+"""
 function set_training_true() 
     @eval Flux.istraining() = true
     @eval Zygote.@adjoint Flux.istraining() = true, _ -> nothing
 end
 
+# one_hot_encode: Utility to return one_hot array for provided index with size same as that of `preds`
 function one_hot_encode(preds, idx)
   one_hot = zeros(eltype(preds), size(preds)[1], 1)
   one_hot[idx.I[1] ,1] = 1
   one_hot |> gpu
 end
 
+# get_topk: Utility to return top_k predicted classes with their probabilities
 function get_topk(probs; k = 5)
   T = eltype(probs)
   prob = Array{T, 1}()
@@ -30,6 +44,11 @@ function get_topk(probs; k = 5)
   (prob, idx)
 end
 
+"""
+    save_gradient_images(gradient, file_name; gray=false)
+
+Saves the gradient as an RGB image by default. If `gray` is set to true, gradients are sasved as a gray image with the provided `filename`.
+"""
 function save_gradient_images(gradient, file_name; gray=false)
   gradient = gradient .- minimum(gradient)
   gradient ./= maximum(gradient)
@@ -43,12 +62,24 @@ function save_gradient_images(gradient, file_name; gray=false)
   save(file_name, img)
 end
 
+
+"""
+    positive_negative_saliency(gradient)
+
+Returns a tuple of positive and negative saliency maps based on the provided gradients.
+"""
 function positive_negative_saliency(gradient)
   pos_saliency = max.(zero(gradient), gradient) ./ maximum(gradient)
   neg_saliency = max.(zero(gradient), -gradient) ./ maximum(-gradient)
   (pos_saliency, neg_saliency)
 end
 
+
+"""
+    image_to_arr(img; preprocess = true)
+
+Convert an RGB type image to an array. If `preprocess` is set as `true`, subtract the mean from the image array.  
+"""
 function image_to_arr(img; preprocess = true)
   local x = img
   x = Float32.(channelview(img))
@@ -59,6 +90,11 @@ function image_to_arr(img; preprocess = true)
   x = reshape(x, size(x,1), size(x,2), size(x,3), 1) * 255 |> gpu
 end
 
+"""
+    load_image(path, resize = false; size_save = true)
+
+Function to load the images from the specified `path`. Returns as an array of image. If `resize` is set as `true`, resize the image to shape (224, 224). 
+"""
 function load_image(path, resize = false; size_save = true)
   img = load(path)
   if(size_save)
@@ -71,14 +107,24 @@ function load_image(path, resize = false; size_save = true)
   end
 end
 
-function generate_image(x, resize_original = false)
+"""
+     generate_image(x; resize_original=false, original_size=Nothing)
+
+Save the image as an RGB image defined the by the array `x`. If `resize_original` is set to be true, then the image can be resized to the `original_size`. If not provided explicitly, then the image is resized to the `original_size` of the image saved while loading the image. 
+"""
+function generate_image(x; resize_original = false, original_size=Nothing)
   x = reshape(x, size(x)[1:3]...)/255 |> cpu
   x = x .+ im_mean
   x = permutedims(x, [3,2,1])
   x .-= minimum(x)
   x ./= maximum(x)
   if resize_original
-    return imresize(colorview(RGB, x), original_size)
+    if original_size!=Nothing
+        return imresize(colorview(RGB, x), original_size)
+    else
+        global original_size
+        return imresize(colorview(RGB, x), original_size)
+    end
   else
     return colorview(RGB, x)
   end
@@ -94,8 +140,18 @@ function arr2rgb_normalize(a)
     colorview(RGB{eltype(a)},a)
 end
 
-save_image(path, x) = save(path, generate_image(x, true))
+"""
+    save_image(path, x)
 
+Save the image defined by `x` to the specified `path`.
+"""
+save_image(path, x) = save(path, generate_image(x; resize_original = true))
+
+"""
+    apple_colormap(org_img, activation_map; map_alpha=0.4, process_img=true)
+
+Creates a `rainbow` based colormap for the image, which is then applied onto `org_img`(original image) with different transparency to generate the combined heatmap with the `org_img` (original image). Function returns a tuple of `combined colormap` and `activation map alone based heatmap`.
+"""
 function apply_colormap(org_img, activation_map; map_alpha=0.4, process_img=true)
     cm = cgrad(:rainbow)
     no_trans_heat_map = RGB.(cm[activation_map])
@@ -104,6 +160,6 @@ function apply_colormap(org_img, activation_map; map_alpha=0.4, process_img=true
         org_img = rgb2arr(org_img)
     end
     x = org_img*0.7 + heat_map*0.3
-    return arr2rgb_normalize(x), heatmap
+    return (arr2rgb_normalize(x), heatmap)
 end
 
--- src/vanilla_gradient.jl
+++ src/vanilla_gradient.jl
@@ -1,5 +1,18 @@
-############################################################### Viz Backpropagation ####################################
 
+"""
+    viz_backprop(img, model; top_k=1, target_based=false, target_index)
+
+Returns gradient generated with Vanilla Backpropagation for `top_k` classes. It’s the original saliency map algorithm for supervised deep learning from [Simonyan et. al. (2013)](https://arxiv.org/abs/1312.6034). These gradients can further be visualized using utilty functions.
+
+# Arguments
+ - `img`: source image for which gradients are to be calculated
+ - `model`: model to be used for visualization
+ - `top_k=1`: gradients are returned for `top_k` classes as per predictions by the model.
+ - `target_based=false`: if set to `true`, function returns the maximizing gradient for the specified `target_index` class along with gradient for `top_k` classes.
+ - `target_index`: defines the target class, for which the maximizing gradients are to be calculated. Required when `target_based` is set to `true`. 
+
+The function returns an of gradients for `top_k=1` predicted classes by default. If `target_based` is set to `true` and `target_index` is specified, the function returns a tuple: `(gradients_for_top_k_classes, gradient_for_target_class)`.
+"""
 function viz_backprop(img, model; top_k=1, target_based=false, target_index=-1)
     model_ = model[1:end-1] |> gpu
     set_training_false()
@@ -21,14 +34,33 @@ function viz_backprop(img, model; top_k=1, target_based=false, target_index=-1)
     grads
 end    
 
-############################################################# Integrated Backpropagation #################################### 
 
+# Integrated Backpropagation 
+
+"""
+    stepped_images(img, steps)
+
+Utility function for `viz_integrated_gradient`. Generate scaled images, ie. images multiplied by a factor `i` defined in the range of `0` to `1.0` over equal intervals of `1/steps`. 
+"""
 function stepped_images(img, steps)
     step_size = 1.0/steps
     imgs_in = [Float32.(img.*i) for i in 0+step_size:step_size:1.0]
     return imgs_in
 end
 
+"""
+    viz_integrated_gradient(img, model, steps; target_based=false, target_index)
+
+Produces gradients generated with integrated gradients from the image scaled over `steps`. This functions uses the `viz_backprop` for the calculation of gradients for each of scaled image and then returns integrated gradients as the average over all the scaled images.
+
+# Arguments 
+ - `img`: source image on which the gradient are to be calculated.
+ - `model`: model to be used for visualization
+ - `steps`: number of steps between 0 to 1.0 for which the image is to be scaled while calculating integrated gradients.
+ - `target_based=false`: if set `true`, the integrated gradients are evaluated for the specified `target_index`.
+ - `target_index`: index of the target class for which the maximized gradients are to be evaluated. Necessary condition when `target_based` is set as `true`.
+ 
+"""
 function viz_integrated_gradient(img, model, steps; target_based=false, target_index=-1)
     imgs_in = stepped_images(img, steps)
     integrated_gradient = zeros(Float32,size(img)...)
